24e71cda2e2ca2f5ee51e16b14fcef96116cae27
Mihriban Minaz <mihri@unumotors.com>
2017-05-09 18:22:37 +0200

    Implement division of the pages and css properties

--- javascript/someScripts.js
+++ javascript/someScripts.js
@@ -2,8 +2,14 @@ function showItem(item) {
     document.getElementById(item).style.display = 'block';
     if (item == 'aboutId') {
         document.getElementById('workId').style.display = 'none';
+        document.getElementById('portfolioButton').style.opacity = "0.5";
+        document.getElementById('aboutButton').style.opacity = "1.0";
+        document.getElementById('half').style.backgroundColor = '#59beb6';
     }
     else {
         document.getElementById('aboutId').style.display = 'none';
+        document.getElementById('aboutButton').style.opacity = "0.5";
+        document.getElementById('portfolioButton').style.opacity = "1.0";
+        document.getElementById('half').style.backgroundColor = '#f46842';
     }
 }
